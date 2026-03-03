e47e033eede66759842db15b5708c61d78906f67
Hoaqim <michaldomasik@wp.pl>
2026-03-03 13:33:05 +0100

    [iac] added nginx to terraform

--- terraform/deployment/main.tf
+++ terraform/deployment/main.tf
@@ -14,6 +14,31 @@ terraform {
 resource "kind_cluster" "default"{
     name = "platform-cluster"
     wait_for_ready = true
+    
+    kind_config {
+        kind       = "Cluster"
+        api_version = "kind.x-k8s.io/v1alpha4"
+
+        node {
+        role = "control-plane"
+        
+        kubeadm_config_patches = [
+            "kind: InitConfiguration\nnodeRegistration:\n  kubeletExtraArgs:\n    node-labels: \"ingress-ready=true\"\n"
+        ]
+
+        extra_port_mappings {
+            container_port = 80
+            host_port      = 80
+            protocol       = "TCP"
+        }
+
+        extra_port_mappings {
+            container_port = 443
+            host_port      = 443
+            protocol       = "TCP"
+        }
+        }
+    }
 }
 
 provider "helm" {
@@ -33,4 +58,14 @@ resource "helm_release" "argocd" {
     namespace   = "argocd"
     create_namespace = true
     version = "9.4.1"
+}
+
+resource "helm_release" "ingress_nginx" {
+    name             = "ingress-nginx"
+    repository       = "https://kubernetes.github.io/ingress-nginx"
+    chart            = "ingress-nginx"
+    namespace        = "ingress-nginx"
+    create_namespace = true
+
+    values = [file("values.yaml")]
 }
